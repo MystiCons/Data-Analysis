68e194e10b67ba1da2144b58e7754716e73f89a4
Niko Liimatainen <K8908@student.jamk.fi>
2017-06-01 15:33:47 +0300

    created a file for scritps

---
+++ scripts/xml_parsing.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+xml_parser.py
+
+Created:  31.5.2017 by Niko Liimatainen
+Modified: 1.6.2017 by Niko Liimatainen
+
+
+Simple XML parser and .json converter.
+
+"""
+
+
+import xml.etree.ElementTree as etree
+import json
+
+root = etree.parse('./Data/weatherDaily').getroot()  # gets the local XML file
+# and it's root for parsing
+
+i = 0
+
+
+dataFile = open('./Data/Weather-Daily_Parsed.json', 'w')
+
+for child in root:  # loop for going through the proper nested lists in the
+    # XMl-file
+    data = {"Time": root[i][0][1].text,
+            "Type": root[i][0][2].text,
+            "Value": root[i][0][3].text}
+    # setting the data as a dict for better .json compability 
+    i += 1
+    dump = json.dumps(data)  # converting data dict to .json dumb
+    dataFile.write(dump + "\n")  # writing the dump to file with newline
+
+
+dataFile.close()
+
